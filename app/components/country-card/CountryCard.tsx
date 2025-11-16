75b3dcd4639d505344e8eaa2d8e8cad3a99b7210
Koumba KEITA <koumba.keita@epitech.eu>
2025-11-16 16:22:37 -0500

    WIP CountryCard component

--- app/components/country-card/CountryCard.tsx
+++ app/components/country-card/CountryCard.tsx
@@ -1,19 +1,41 @@
+import { KoumThemeColor, Label } from 'koum-ui';
 import { Country } from '../../types/Country';
 import './country-card.scss';
+import {
+    Continent,
+    ContinentColor,
+    continentColorMap,
+} from '@/app/types/ContinentColor';
 
 interface Prop {
     country: Country;
 }
+
 export const CountryCard = ({ country }: Prop) => {
     return (
-        <div
-            className="country-card"
-            style={{
-                backgroundImage: `url(/countries/${country.cca3}.jpg)`,
-            }}
-        >
-            <h1>{country.name.common}</h1>
-            <h1>{country.cca3}</h1>
+        <div className="country-card">
+            <div
+                className="card-image"
+                style={{
+                    backgroundImage: `url(/countries/${country.cca3}.jpg)`,
+                }}
+            ></div>
+            <div className="card-info">
+                <h1 className="country-name">{country.name.common}</h1>
+                <Label
+                    additionalClass="country-continent"
+                    label={country.continents[0]}
+                    size="small"
+                    color={
+                        continentColorMap[
+                            country.continents[0].replace(' ', '') as Continent
+                        ]
+                    }
+                ></Label>
+                <p className="country-population">
+                    Population: {country.population}
+                </p>
+            </div>
         </div>
     );
 };
---
+++ app/types/ContinentColor.ts
@@ -0,0 +1,27 @@
+import { KoumThemeColor } from 'koum-ui';
+
+export type Continent =
+    | 'Africa'
+    | 'Asia'
+    | 'Europe'
+    | 'NorthAmerica'
+    | 'Oceania'
+    | 'SouthAmerica';
+
+export const ContinentColor = {
+    Africa: 'primary',
+    Asia: 'secondary',
+    Europe: 'tertiary',
+    NorthAmerica: 'info',
+    Oceania: 'warning',
+    SouthAmerica: 'neutral',
+} as const satisfies Record<string, KoumThemeColor>;
+
+export const continentColorMap: Record<Continent, KoumThemeColor> = {
+    Africa: ContinentColor.Africa,
+    Asia: ContinentColor.Asia,
+    Europe: ContinentColor.Europe,
+    NorthAmerica: ContinentColor.NorthAmerica,
+    Oceania: ContinentColor.Oceania,
+    SouthAmerica: ContinentColor.SouthAmerica,
+};
